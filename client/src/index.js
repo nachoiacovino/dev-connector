8f18d0a0b6f4a9ff50397b795d70f8e48aca6f12
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-12-02 19:47:23 +0100

    Setup redux boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,23 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
+import { persistor, store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ client/src/redux/store.js
@@ -0,0 +1,27 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger();
+  middlewares.push(logger);
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+sagaMiddleware.run(rootSaga);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
