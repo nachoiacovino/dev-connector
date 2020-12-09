6b1b21d9dbbb12d9bd8aea37bd49b3ef0f72b6d0
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-12-09 03:44:12 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDb = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,12 +12,20 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
